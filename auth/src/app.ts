9e0b2c11fb84a5b2b79767e7448514bde67710a9
Julian Simpli <simplijulian@gmail.com>
2025-06-20 11:37:39 -0300

    feat: update config for prod build

---
+++ auth/src/app.ts
@@ -0,0 +1,42 @@
+import express from 'express'
+import cookieSession from 'cookie-session'
+
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
+
+const app = express()
+// config for our ingress nginx proxy service
+// req.ip will have real client info
+app.set('trust proxy', true)
+app.use(express.json())
+// will create req.session object
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+    httpOnly: true,
+  })
+)
+
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
+
+// handling not found routes
+// Express 4
+// app.use((req, res, next) => {
+//   next(new NotFoundError())
+// })
+// Express 5
+app.use((req, res) => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
